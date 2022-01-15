e912d4516dacb1c236857f39c842fa8caa134975
michaaalD <93281456+michaaalD@users.noreply.github.com>
2022-01-15 22:28:50 +0100

    Update 22.12.c

--- 22.12.c
+++ 22.12.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <math.h>
+ 
 
-#define BRAK_ZNAKU 0
- 
- 
 typedef struct  el_stosu{
     int wartosc;
     struct el_stosu* next;
@@ -50,144 +50,249 @@ void empty(el_stosu **head)
     {
         *head = (*head)->next;
     }
+    head = NULL;
+    
     printf("Stos Pusty\n");
 }
 
 int print(el_stosu **head){
-  if(*head != NULL){
-    el_stosu *display_top;
-    display_top = *head;
-    int x=display_top->wartosc;
-    printf("Szczyt stosu: %d\n",x);
-    return 0;
    
+   if(*head != NULL){
+      el_stosu *display_top;
+      display_top = *head;
+      int x=display_top->wartosc;
+      printf("Szczyt stosu: %d\n",x);
+      return x;
   }
   else{
     printf("Stos pusty\n");
     return 1;
   }
-  }
+}
+
 
 
 int print_FULL(el_stosu ** head){
 
     el_stosu *display_all;
     display_all = *head;
+    
     if(display_all!= NULL)
     {
-        printf("Stos: ");
+        printf("Elementy Stosu:\n");
         do
         {   
             int x = display_all->wartosc;
-            printf("%d ",x);
+            printf("%d\n",x);
             display_all = display_all->next;
         }
         while (display_all!= NULL);
-        printf("\n");
+        return 0;
     }
     else
     {
         printf("Stos pusty\n");
+        return 1;
     }
-
- // new_element->;
 }
+
+
+/*
+void copy_top(el_stosu ** head){
+   
+  if(*head != NULL){
+      el_stosu *display_top;
+      display_top = *head;
+      int *temp= malloc(sizeof(int));
+      temp = display_top->wartosc;
+      printf("Szczyt stosu: %d\n",x);
+      return 0;
+    
+    }
+    else{
+      printf("Stos pusty\n");
+      return 1;
+    }
+  }
+*/
+
 
 int main()
 { 
-  el_stosu* stos = NULL;
+  el_stosu* stos = NULL;      //zainicjowanie stosu
 
   char oper =' ';             //zmienna operatora
-  int arg;                    //zmienna aktualnego argumentu
+  float arg;                  //zmienna aktualnego argumentu
   int n=0;                    //liczba argumentow
-
+  
+  printf("-------------------------------------------------------------------------------\n");
+  printf("Kalkulator RPN\n");
+  printf("-------------------------------------------------------------------------------\n");
+  printf("Instrukcja podawania argumentów: \n");
+  printf("-------------------------------------------------------------------------------\n");
+  printf("1 liczba\n");
+  printf("2 liczba\n");
+  printf("znak operacji:\n");
+  printf("+(jako '-+'), -(jako '--'), '/', '*', '%'(modulo), '^'(potegowanie),\n");
+  printf(" t(print top), p(pop), c(wyczysc stos), f(print stos), r(odwroc stos), d(duplikat topu), q(koniec)\n");
+  printf("-------------------------------------------------------------------------------\n");
 
   while(oper != 'q'){
-    if(scanf("%d",&arg)){
-      printf("Wczytano liczbe: %d\n",arg);
-      push(&stos,arg);
-      n++;
-    }
-    else{
-      oper = getc(stdin);
-      printf("Wczytano znak: >%c<\n",oper);
-    }
-  if(n>1 && oper != ' '){
-    switch(oper){
-      case '+':
-      {
-        int a= pop(&stos);
-        int b = pop(&stos);
-        float c = b+a;
-        printf("Wynik: %.2f\n",c);
-        push(&stos,c);
-        n--;
-        break;
+    
+    if(scanf("%f",&arg)){                     //pobranie argumentow liczbowych
+        printf("Wczytano liczbe: %.2f\n",arg);
+        push(&stos,arg);                      //umieszczenie na stosie
+        n++;                                  //zwiekszenie licznika ilosci argumentow
       }
-      case '-':
-      {
-        int a= pop(&stos);
-        int b = pop(&stos);
-        float c = b-a;
-        printf("Wynik: %.2f\n",c);
-        push(&stos,c);
-        n--;
-        break;
-        }
-      case '/':
-      {
-        int a= pop(&stos);
-        int b = pop(&stos);
-        float c = (float)b/(float)a;
-        printf("Wynik: %.2f\n",c);
-        push(&stos,c);
-        n--;
-        break;
+    else{ 
+        oper = getc(stdin);                   //pobranie operatora
+        printf("Wczytano znak: >%c<\n",oper);
       }
-      case '*':
-      {
-        int a= pop(&stos);
-        int b = pop(&stos);
-        float c = (float)b*(float)a;
-        printf("Wynik: %.2f\n", c);
-        push(&stos,c);
-        n--;
-        break;
-      }
-      case 'c':
-      {
-        empty(&stos);
-        n=0;
-        break;
-      }
-      case 'p':
-      {
-        print(&stos);
-        break;
-      }
-      case 'f':
-      {
-        print_FULL(&stos);
-        break;
-      }
-      default:
+
+  
+    if(n>1 && oper != ' '){
+    
+      switch(oper){
+        case '+':
+          {
+            float a= pop(&stos);
+            float b = pop(&stos);
+            float c = b+a;                 //pomocnicza c do operacji arytmetycznych
+            printf("Wynik: %.2f\n",c);
+            push(&stos,c);
+            n--;                           //po kazdej operacji arytmetycznej licznik elementow -1
+            oper=' ';                      //po kazdej operacji wyzerowanie operatora
+          break;
+          }
+        case '-':
+          { 
+            float a= pop(&stos);
+            float b = pop(&stos);
+            float c = b-a;
+            printf("Wynik: %.2f\n",c);
+            push(&stos,c);
+            n--;
+            oper=' ';
+            break;
+            }
+        case '/':
+          {
+            float a= pop(&stos);
+            float b = pop(&stos);
+            if(a!=0){
+              float c = b/a;
+              printf("Wynik: %.2f\n",c);
+              push(&stos,c);
+            }
+            else{
+              printf("Nie mozna dzielic przez 0\n");
+            }
+            n--;
+            oper=' ';
+            break;
+          }
+        case '*':
+          {
+            float a= pop(&stos);
+            float b = pop(&stos);
+            float c = b*a;
+            printf("Wynik: %.2f\n", c);
+            push(&stos,c);
+            n--;
+            oper=' ';
+            break;
+          }
+        case '%':
+          {
+            float a= pop(&stos);
+            float b = pop(&stos);
+            int c = (int)b % (int)a;            //modulo musi miec liczby stalopozycyjne, wiec zmieniam typ zmiennej z float na int, liczby beda mialy anulowana czesc po przecinku, jezeli ja maja, a nastepnie zostanie zwrocona reszta z dzielenia tych liczb
+            printf("Wynik: %d\n", c);
+            push(&stos,c);
+            n--;
+            oper=' ';
+            break;
+          }
+        case '^':
         {
-          if(oper!='q')
-          printf("Invalid\n");
-          n--;
+            float a= pop(&stos);
+            float b = pop(&stos);
+            float c = pow(b,a);
+            printf("Wynik: %.2f\n", c);
+            push(&stos,c);
+            n--;
+            oper=' ';
+            break;
+        }
+        case 'c':
+          {
+            empty(&stos);                       
+            free(stos);                         //zwolnienie pamieci dla stosu
+            stos=NULL;                          //przywrocenie aby stos znow wskazywal na "nic"
+            n=0;                                //n=0 bo zdjelismy wszystie argumenty
+            oper=' ';
+            break;
+          }
+        case 't':
+          {
+            print(&stos);                     
+            oper=' ';
+            break;
+          }
+        case 'f':
+          {
+            print_FULL(&stos);
+            oper=' ';
+            break;
+          }
+        case 'p':
+          {
+            pop(&stos);
+            oper = ' ';
+            break;
+          }
+        case 'r':
+        {
+          el_stosu* stos_temp1 = NULL;              //pomocnicze stosow do zmaiany kolejnosci elementow
+          el_stosu* stos_temp2 = NULL;
+          
+          push(&stos_temp1,pop(&stos));             //push elementow ze stosu glownego na stos temp1 
+          push(&stos_temp1,pop(&stos));
+          free(stos);                               //zwolnienie pamieci stosu glownego
+
+          push(&stos_temp2,pop(&stos_temp1));       //push ze stosu temp1 na stos temp2
+          push(&stos_temp2,pop(&stos_temp1));
+          free(stos_temp1);                         //zwolnienie pamieci stosu temp1
+
+          push(&stos,pop(&stos_temp2));             //push elementow ze stosu temp2 na stos glowny
+          push(&stos,pop(&stos_temp2));
+          free(stos_temp2);                         ////zwolnienie pamieci stosu temp2
+
+
+          printf("Odwrocono stos\n");
+          oper=' ';
           break;
         }
-        oper=' ';
+        default:
+          {
+            if(oper!='q')
+            printf("Nieprawidłowy znak\n");
+            n--;
+            oper=' ';
+            break;
+          }
+        }
+      }
+    else if(n < 2 || oper != 'c' || oper != 'p' || oper != 'f' || oper != 't'){
+      printf("Wymagane 2 argumenty liczbowe\n");              //sprawdzenie czy sa 2 argumenty liczbowe
     }
-  }
-
     
-
-    }
+}
   printf("Koniec Programu\n");
   
 
   
   return 0;
 }
+
 
