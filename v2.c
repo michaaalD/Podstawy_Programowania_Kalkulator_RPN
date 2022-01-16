f93b0bedb8a3a8e4684bd1d97260a8eb747df262
michaaalD <93281456+michaaalD@users.noreply.github.com>
2022-01-16 19:33:54 +0100

    Update v2.c

--- v2.c
+++ v2.c
@@ -1,91 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
- 
- 
-typedef struct  el_stosu{
-    int wartosc;
-    struct el_stosu* next;
-} el_stosu;
- 
-el_stosu new_el_stosu(int wartosc){
-    el_stosu el = {wartosc, NULL};
-    return el;
-}
- 
-void push(el_stosu **head, int wartosc){
-    el_stosu* new_element = (el_stosu*)malloc(sizeof(el_stosu)); //zaalokowanie pamieci dla stosu 
- 
-    new_element->wartosc = wartosc;
-    new_element->next = *head;
- 
-    *head = new_element;
-}
- 
 
-int pop(el_stosu **head)
-{
-    if(*head != NULL){
-        int wartosc;
-        el_stosu *old_head = *head; //aktualny element na szczycie stosu, jako old_head
- 
-        *head = (*head)->next;
-        wartosc = old_head->wartosc;
- 
-        free(old_head); //zwolnienie pamieci gdzie byl wskaznik na zdjety element
-        return wartosc;
+typedef struct{
+    int *tab; //wsk na adres dynamicznej listy
+    int top;  
+    int rozmiar;
+} Stos;
+
+Stos* inicjuj(int rozmiar){
+    Stos *wsk = (Stos*)malloc(sizeof(Stos));
+    if(wsk == NULL){
+        printf("Stos niezainicjowany\n");
+        exit(1);
     }
-    else{
-        printf("Nie mozna zdjac elementu z pustego stosu\n");
+    wsk->tab= (int*) malloc(sizeof(int) * rozmiar);
+    if(wsk->tab == NULL){
+        printf("Nie mona zainicjalizowac stosu\n");
+        exit(1);
+    }
+    wsk->top = -1; //top indeks -1, w miare zwiekszania stosu, indeks zwieksza sie, wiec 1 element ma indeks 0
+    wsk->rozmiar = rozmiar;
+
+    return wsk;
+
+}
+void push(Stos *wsk, int x){
+    if(wsk->top == wsk->rozmiar -1){ //zebyn nie przepelnic stosu
+    int* temp =(int*)malloc(sizeof(int)*(*wsk).rozmiar *2); //lokowanie pamieci
+    if(temp == NULL){
+        printf("Nie udalo sie zaalokowac pamieci\n");
+    }
+    for(int i=0; i<wsk->rozmiar -1; i++){ //skopiowanie tablicy tab do tymczasowej tablicy temp
+        temp[i]=wsk->tab[i];
+    }
+    free(wsk->tab);   // zwolnienie pamieci na tab
+    wsk->tab=temp;    //temp ma adres nowo zalokowanej tablicy
+    wsk->rozmiar *=2;
+    }
+    wsk->tab[++wsk->top] = x; //zwiekszenie indeksu topu i umieszczenie tam x
+}
+int pop(Stos *wsk){
+    if(wsk->top == -1){ //sprawdzenie czy top = -1, wtedy stos jest pusty
+        printf("Stos jest pusty\n");
         return 1;
     }
+    else
+        return wsk->tab[wsk->top--];
 }
 
-void empty(el_stosu **head)
-{
-    el_stosu *old_head = *head;
-    while(*head != NULL)
-    {
-        *head = (*head)->next;
+int empty_stack(Stos *wsk){ 
+    if(wsk->top == -1){ //sprawdzenie czy top = -1, wtedy stos jest pusty
+        printf("Stos jest pusty\n");
+        return 1;
     }
-    printf("Stos Pusty\n");
-}
-
-int main()
-{ 
-  el_stosu* stos = NULL;
-  char oper=' ';
-  int arg;
-  int n;
-
-  while(oper != 'q'){
-    if(scanf("%d",&arg)){
-      printf("Wczytano liczbe: %d\n",arg);
-      push(&stos,arg);
-      n++;
-    }
-    else{
-      oper = getc(stdin);
-      printf("Wczytano znak: >%c<\n",oper);
-    }
-    if(n>=2)
-    {
-    switch(oper){
-      case '+':{
-        int a = pop(&stos);
-        int b = pop(&stos);
-        printf("Wynik: %d",a+b);
-        push(&stos,a+b);
-        break;
-      }
-      default:
-        {
-          printf("Invalid\n");
-          break;
+    else
+        printf("Elementy stosu:\n");
+        for(int i=0; i< wsk->rozmiar ; i++){ 
+            if(wsk->top != -1){
+            printf(" %d\n",wsk->tab[wsk->top]);
+            wsk->top--;
+            }
         }
-      n=1;
+}
+
+int print_top(Stos* wsk){
+    if(wsk->top == -1){ //sprawdzenie czy top = -1, wtedy stos jest pusty
+        return 1;
     }
-    }
-  }
+    else
+        printf("Top stosu: %d\n",wsk->tab[wsk->top]);       
+}
+
+int main(){
+    
+
+    Stos *stack = inicjuj(5);
+
+    push(stack,3);
+    push(stack, 7);
+    
+    printf("%d",pop(stack) + pop(stack));
+    
+
     return 0;
 }
